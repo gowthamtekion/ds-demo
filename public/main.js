185a01fdbbf2e9e4b3726f6755e08906355e95f0
Gowthaman <gthirunavukkarasu@tekion.com>
2023-05-16 16:34:31 +0530

    main js added

---
+++ public/main.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const fs = require("fs");
+
+const dirPathPages = path.join(__dirname, "../src/pages/content");
+let pagelist = [];
+
+const getPages = () => {
+  fs.readdir(dirPathPages, (err, files) => {
+    if (err) {
+      return console.log("Failed to list contents of directory: " + err);
+    }
+    files.forEach((file, i) => {
+      let page;
+      fs.readFile(`${dirPathPages}/${file}`, "utf8", (err, contents) => {
+        page = {
+          content: contents,
+        };
+        pagelist.push(page);
+        let data = JSON.stringify(pagelist);
+        fs.writeFileSync("src/pages.json", data);
+      });
+    });
+  });
+  return;
+};
+
+getPages();
